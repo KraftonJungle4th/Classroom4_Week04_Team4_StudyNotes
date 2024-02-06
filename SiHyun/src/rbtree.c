aaa7a08b81de9a250929b6b1867ade3840c92378
pongpongie <moorow0729@hufs.ac.kr>
2024-02-06 09:01:47 +0000

    feat: find.c

--- SiHyun/src/rbtree.c
+++ SiHyun/src/rbtree.c
@@ -4,54 +4,59 @@
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  node_t *root = (node_t *)calloc(1, sizeof(node_t));
   node_t *nil = (node_t *)calloc(1, sizeof(node_t));
-  root = NULL;
-  nil = NULL;
-  p->root = root;
+  nil->color = RBTREE_BLACK;
   p->nil = nil;
+  p->root = nil;
   return p;
 }
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
-  // 할일: 트리노드의 메모리를 회수하는것?
   free(t);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  node_t z;
-  z.key = key;
-  z.parent = NULL;
-  z.left = NULL;
-  z.right = NULL;
+  node_t *z = (node_t *)calloc(1, sizeof(node_t));
+
+  z->key = key;
+
   node_t* y = t->nil;
   node_t* x = t->root;
+
   while (x != t->nil){
       y = x;
-      if (z.key < x->key){
+      if (z->key < x->key){
         x = x->left;
       } else {
         x = x->right;
       }
   }
-  z.parent = y;
+  z->parent = y;
   if (y == t->nil){
-    t->root = &z;
-  } else if(z.key < y->key){
-    y->left = &z;
+    t->root = z;
+  } else if(z->key < y->key){
+    y->left = z;
   } else{
-    y->right = &z;
+    y->right = z;
   }
-  z.left = t->nil;
-  z.right = t->nil;
-  z.color = RBTREE_RED;
-  // RB-Insert-Fixup(t, &z)
-  return t->root;
+  z->left = t->nil;
+  z->right = t->nil;
+  z->color = RBTREE_RED;
+  // RB-Insert-Fixup(t, z)
+
+  return t->root; 
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  t->root;   
+  // TODO: implement find
+  node_t* x = t->root;
+
+  while (x != t->nil && key != x->key);
+    if (key < x->key)
+      x = x->left;
+    else (x = x->right);
+  return x; 
 
 }
 
@@ -66,8 +71,8 @@ node_t *rbtree_max(const rbtree *t) {
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
-
-    return 0;
+  // TODO: implement erase
+  return 0;
 }
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
