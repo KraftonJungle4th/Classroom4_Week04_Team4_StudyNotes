d36ed30fa24c47148b05dc6ec30736af2072b705
UkdongKim <edisen315@gmail.com>
2024-02-06 12:20:14 +0000

    테스트 전 끝!

--- DongUk/src/rbtree.c
+++ DongUk/src/rbtree.c
@@ -81,17 +81,37 @@ void insert_fixup(rbtree * t, node_t * z){
 
 rbtree *new_rbtree(void) {
   rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // node_t *root = (node_t *)calloc(1, sizeof(node_t));
-  // node_t *nil = (node_t *)calloc(1, sizeof(node_t));
-  p->root = NULL;
-  p->nil = NULL;
+  node_t *nil = (node_t *)calloc(1, sizeof(node_t));
+  nil->color = RBTREE_BLACK;
+  p->nil = nil;
+  p->root = nil;
   return p;
+}
+
+// 추가함수
+void postorder(rbtree * t, node_t * target){
+  if (target->left != t->nil){
+      postorder(t, target -> left);
+  }
+
+  if (target->right != t->nil){
+      postorder(t, target -> right);
+  } 
+
+  free(target);
 }
 
 void delete_rbtree(rbtree *t) {
   // TODO: reclaim the tree nodes's memory
+
+  if (t->root != t->nil){
+    postorder(t, t->root);
+  }
+
+  free(t->nil);
   free(t);
 }
+
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
   // TODO: implement insert
@@ -124,19 +144,24 @@ node_t *rbtree_insert(rbtree *t, const key_t key) {
   z->left = t->nil;
   z->right = t->nil;
   z->color = RBTREE_RED;
-  // insert_fixup(t, z)
+  insert_fixup(t, z);
 
   return t->root; 
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
-  if (t->root->key != key) {
-    return NULL;
-  }
-  else {
-    return t->root;
+   // TODO: implement find
+  node_t* x = t->root;
+
+  while (x != t->nil && key != x->key) {
+    if (key < x->key) {
+      x = x->left;
     }
+    else {
+      x = x->right;
+    } 
+  }
+  return x == t->nil ? NULL : x;
 }
 
 node_t *rbtree_min(const rbtree *t) {
@@ -160,12 +185,40 @@ node_t *rbtree_max(const rbtree *t) {
   return cur;
 }
 
+node_t *find_successor(const rbtree *t, node_t* target) {
+  // TODO: implement find
+
+  while(target->left != t->nil){
+    target = target->left;
+  }
+
+  return target;
+}
+
+
 int rbtree_erase(rbtree *t, node_t *p) {
   // TODO: implement erase
   return 0;
 }
 
+
+// 추가함수
+void inorder(rbtree * t, node_t * target, int index, size_t size, key_t *arr){
+  if (target == t->nil){
+    return;
+  }
+      inorder(t, target -> left, index, size, arr);
+      if (index < size){
+        arr[index++] = target->key;
+      }
+      inorder(t, target -> right, index, size, arr);
+}
+
+
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
   // TODO: implement to_array
+
+  inorder(t, t->root, 0, n, arr);
+
   return 0;
 }
